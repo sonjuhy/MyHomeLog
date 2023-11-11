5528fa7f4c5119bd9461b2d09e124871d15efb85
Red Hat <sonjuhy@gmail.com>
2023-11-11 16:35:39 +0900

    ✨ ADD MongoDB id auto increase feature
    - ADD MongoDB id auto increase feature

--- src/main/java/com/myhome/log/db/entity/LogDefaultEntity.java
+++ src/main/java/com/myhome/log/db/entity/LogDefaultEntity.java
@@ -1,4 +1,29 @@
 package com.myhome.log.db.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "default_log")
 public class LogDefaultEntity {
+
+    @Transient
+    public static final  String SEQUENCE_NAME="log_sequence";
+
+    @Id
+    private long id;
+    private boolean type; // error(false), success(true)
+    private String sender; // spring, django
+    private String service; // light(iot), reserve(iot), cloud, fileCheck(cloud)
+    private String day; // send log day yyyy-mm-dd
+    private String time; // send log time hh-mm-ss
+    private String content; // log content
 }
